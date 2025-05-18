21e17a29079f3bf23b957b53c23670bb717b5c59
Nicolás Villegas Castillo <navillegas@miuandes.cl>
2025-05-18 13:40:35 -0400

    Update y coefficient

--- PalletizingRobot.py
+++ PalletizingRobot.py
@@ -37,7 +37,7 @@ class PalletizingRobot:
         # Affine transformation coefficients for Y-coordinate
         # Y_robot = Y_MAPPING_SLOPE * y_cam_pixel + Y_MAPPING_INTERCEPT
         self.Y_MAPPING_SLOPE = -0.1713
-        self.Y_MAPPING_INTERCEPT = -8
+        self.Y_MAPPING_INTERCEPT = -1
         
         self.ANG_MAPPING_SLOPE = 0.9294
         self.ANG_MAPPING_INTERCEPT = 86
