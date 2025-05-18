c6eb11a7effdc3c88309c9c727ba9b8569fde5a1
Nicolás Villegas Castillo <navillegas@miuandes.cl>
2025-05-18 12:53:32 -0400

    Update x mapping

--- PalletizingRobot.py
+++ PalletizingRobot.py
@@ -32,7 +32,7 @@ class PalletizingRobot:
         # Affine transformation coefficients for X-coordinate
         # X_robot = X_MAPPING_SLOPE * x_cam_pixel + X_MAPPING_INTERCEPT
         self.X_MAPPING_SLOPE = 0.5208
-        self.X_MAPPING_INTERCEPT = -130
+        self.X_MAPPING_INTERCEPT = -125
 
         # Affine transformation coefficients for Y-coordinate
         # Y_robot = Y_MAPPING_SLOPE * y_cam_pixel + Y_MAPPING_INTERCEPT
